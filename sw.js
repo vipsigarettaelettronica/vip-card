c6f2de263c3f795e63853230bf177193e761f0ee
vipsigarettaelettronica <vipsigarettaelettronica@gmail.com>
2026-09-18 17:26:43 +0200

    Aggiunge avatar alla cache PWA

--- sw.js
+++ sw.js
@@ -34,10 +34,10 @@ self.addEventListener('notificationclick', event => {
     clients.openWindow(url)
   );
 });
-const CACHE = 'vip-card-v6-1-sms-reads';
+const CACHE = 'vip-card-v6-1-1-avatar';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './privacy.html',
-  './assets/logo-vip.png', './assets/icon-192.png', './assets/icon-512.png',
+  './assets/logo-vip.png', './assets/icon-192.png', './assets/icon-512.png', './assets/federica-avatar.png',
   './admin.html', './admin.js'
 ];
 self.addEventListener('install', event => {
