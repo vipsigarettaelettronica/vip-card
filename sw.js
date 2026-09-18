ee39bb789f55d3b3cb7a498e65993e92d22611ff
vipsigarettaelettronica <vipsigarettaelettronica@gmail.com>
2026-09-18 17:34:54 +0200

    Aggiorna cache avatar corretto

--- sw.js
+++ sw.js
@@ -34,10 +34,10 @@ self.addEventListener('notificationclick', event => {
     clients.openWindow(url)
   );
 });
-const CACHE = 'vip-card-v6-1-1-avatar';
+const CACHE = 'vip-card-v6-1-2-avatar-fix';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './privacy.html',
-  './assets/logo-vip.png', './assets/icon-192.png', './assets/icon-512.png', './assets/federica-avatar.png',
+  './assets/logo-vip.png', './assets/icon-192.png', './assets/icon-512.png', './assets/federica-avatar.png?v=2',
   './admin.html', './admin.js'
 ];
 self.addEventListener('install', event => {
